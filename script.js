8682afa98f88dbfc7c4aa37181b210b821fbb989
Josh Beaver <josh.elvn11@gmail.com>
2023-08-11 16:29:47 +0100

    feat: added background audio, particles and entry options dialog

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* ----- Mobile Menu Modal ---- */
+
 const modal = document.getElementById("menu-modal");
 const btnOpenModal = document.getElementById("mobile-nav-btn");
 const btnCloseModal = document.getElementById("mobile-nav-btn-close");
@@ -22,85 +24,24 @@ modalOverlay.addEventListener("click", () => {
   modalOverlay.classList.add("hidden");
 });
 
-let v = document.getElementById("background-video");
-function My_VideoRate() {
-  alert(v.playbackRate);
-}
-function Set_VideoRate() {
-  v.playbackRate = 1;
-}
+/* ----- Entry Options Modal ---- */
 
-Set_VideoRate();
+const entryModal = document.getElementById("entry-modal");
+const backgroundAudio = document.getElementById("background-audio");
+const enableAudio = document.getElementById("enable-audio");
+const enableParticles = document.getElementById("enable-particles");
+const enableHightContrast = document.getElementById("enable-hight-contrast");
+const enterSiteBtn = document.getElementById("enter-site-button");
+const particles = document.getElementById("particles-js");
 
-particlesJS("particles-js", {
-  particles: {
-    number: { value: 50, density: { enable: true, value_area: 800 } },
-    color: { value: "#ffffff" },
-    shape: {
-      type: "circle",
-      stroke: { width: 0, color: "#000000" },
-      polygon: { nb_sides: 5 },
-      image: { src: "img/github.svg", width: 100, height: 100 },
-    },
-    opacity: {
-      value: 0.5,
-      random: false,
-      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
-    },
-    size: {
-      value: 3,
-      random: true,
-      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
-    },
-    line_linked: {
-      enable: false,
-      distance: 915.4257124142767,
-      color: "#ffffff",
-      opacity: 0.4,
-      width: 1,
-    },
-    move: {
-      enable: true,
-      speed: 2,
-      direction: "none",
-      random: false,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: { enable: false, rotateX: 600, rotateY: 1200 },
-    },
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: { enable: true, mode: "repulse" },
-      onclick: { enable: true, mode: "push" },
-      resize: true,
-    },
-    modes: {
-      grab: { distance: 400, line_linked: { opacity: 1 } },
-      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 200, duration: 0.4 },
-      push: { particles_nb: 4 },
-      remove: { particles_nb: 2 },
-    },
-  },
-  retina_detect: true,
-});
-var count_particles, stats, update;
-stats = new Stats();
-stats.setMode(0);
-stats.domElement.style.position = "absolute";
-stats.domElement.style.left = "0px";
-stats.domElement.style.top = "0px";
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector(".js-count-particles");
-update = function () {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+enterSiteBtn.addEventListener("click", () => {
+  if (enableAudio.checked) {
+    backgroundAudio.play();
   }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
+
+  if (!enableParticles.checked) {
+    particles.classList.add("hidden");
+  }
+
+  entryModal.classList.add("hidden");
+});
