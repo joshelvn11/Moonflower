ac83308452c60d71e99946a18aa2c4ca0249d658
Josh Beaver <josh.elvn11@gmail.com>
2023-08-11 02:34:04 +0100

    feat: added hero section
    currently only working on desktop

--- script.js
+++ script.js
@@ -3,9 +3,16 @@
 const modal = document.getElementById("menu-modal");
 const btnOpenModal = document.getElementById("mobile-nav-btn");
 const btnCloseModal = document.getElementById("mobile-nav-btn-close");
+const modalOverlay = document.getElementById("modal-overlay");
 
 console.log(btnOpenModal);
 
 btnOpenModal.addEventListener("click", () => {
   modal.classList.remove("hidden");
+  modalOverlay.classList.remove("hidden");
+});
+
+modalOverlay.addEventListener("click", () => {
+  modal.classList.add("hidden");
+  modalOverlay.classList.add("hidden");
 });
